d81da61a8ca684b1fdcc5d7ff604c5aa12c23f91
Harshofft <hdharsh122@gmail.com>
2025-12-25 22:14:23 +0530

    day 29

--- day10mapfilter.js
+++ day10mapfilter.js
@@ -2,10 +2,50 @@
 
 //for in loop
 const arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (const i in arry) {
-    if (!Object.hasOwn(arry, i)) continue;
+const str = "Hello World";
+for (const i of arry) {
     
-    const element = arry[i];
     
+    const element = i
+    // console.log(str[i]);
+    
+    // console.log(element);
     
 }
+ // map is an kind of object  
+ const map = new Map();
+ map.set('a',1);
+ map.set('b',2);
+ map.set('c',3);
+
+ for (const [key, value] of map) {
+    // console.log(map);
+    // console.log(`${key}:- ${value}`);
+    
+    
+ }
+
+ // methode for object
+ // for in  loop
+ const languages = {
+    js: "javaScript",
+    py: "python",
+    rb: "ruby",
+    java: "java"
+ }
+
+ for (const key in languages) {
+    // console.log(`${key} : ${languages[key]}`);
+    
+    
+ }
+ 
+ const langArry = ["js", "py", "rb", "java"];
+ for (const key in langArry ) {
+
+    
+    const element = langArry[key];
+    
+    console.log(element);
+    
+ }
