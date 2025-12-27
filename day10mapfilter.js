6ce05a6ad8934b3272b44194d784d77023554d4f
Harshofft <hdharsh122@gmail.com>
2025-12-27 12:51:59 +0530

    learn map

--- day10mapfilter.js
+++ day10mapfilter.js
@@ -1,5 +1,7 @@
 // for each , filter , reducer , map , set 
 
+
+
 //for in loop
 const arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const str = "Hello World";
@@ -12,7 +14,11 @@ for (const i of arry) {
     // console.log(element);
     
 }
- // map is an kind of object  
+ // map is an kind of object 
+
+ //********************important note
+
+ // for of loop is for directly give the value if we using on map it can give key and value both  as shown below
  const map = new Map();
  map.set('a',1);
  map.set('b',2);
@@ -27,6 +33,11 @@ for (const i of arry) {
 
  // methode for object
  // for in  loop
+
+ //********imp notes for for in  */
+ 
+ //for in loop is used for object or array and it give the key of the object 
+ // as shown below
  const languages = {
     js: "javaScript",
     py: "python",
@@ -46,6 +57,29 @@ for (const i of arry) {
     
     const element = langArry[key];
     
-    console.log(element);
+   //  console.log(element);
     
  }
+
+ //most important array methods forEach , map , filter , reduce
+ 
+ //forEach
+ const langArry2 = ["js", "py", "rb", "java"];
+
+ langArry2.forEach((val , index, array) => {
+   // console.log(`index is ${index} value is ${val}  and array is ${array}`);
+   
+ })
+  
+ // this we also can use for array of objects
+ const userx = [
+   {name: "john", age: 25, city: "New York"},
+   {name: "alice", age: 30, city: "Los Angeles"},
+   {name: "bob", age: 22, city: "Chicago"},
+   {name: "eva", age: 28, city: "Miami"}
+ ]
+
+ userx.forEach((item) => {
+   console.log(item.name+ `and his age is ${item.age} is from ` + item.city);  
+   
+ })
